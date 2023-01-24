aee2596765d307ce250b94cfaf8e401fd8df990a
Marcos Felipe S.L <marcos.lima52@fatec.sp.gov.br>
2023-01-23 23:11:34 -0300

    forEach exs

--- forEach2.js
+++ forEach2.js
@@ -1,3 +1,9 @@
+Array.prototype.forEach2 = function (callback) {
+  for(let i = 0; i < this.length; i++) {
+    callback(this[i], i, this);
+  }
+}
+
 const aprovados = ['Agatha', 'Aldo', 'Daniel', 'Raquel'];
 
 aprovados.forEach2(function (nome, indice) {
---
+++ forEach3.js
@@ -0,0 +1,6 @@
+const socialNetworks = ["yotube", "facebook", "twitter", "instagram", "linkedin"];
+
+socialNetworks.forEach(function (socialNetwork, index, array) {
+    console.log(index, socialNetwork, array)
+})
+
