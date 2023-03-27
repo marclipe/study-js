264ae50fd2f3e975e1f7617e20157534c4650f8b
Marcos Felipe S.L <marcos.lima52@fatec.sp.gov.br>
2023-03-27 20:32:07 -0300

    module.exports and require

---
+++ funcoes/baseExpoente/funcaoBaseExpoente.js
@@ -0,0 +1,9 @@
+function funcaoBaseExpoente(base, expoente) {
+  console.log(Math.pow(base, expoente));
+};
+
+funcaoBaseExpoente(4,2)
+
+module.exports = {
+  funcaoBaseExpoente,
+}; 
---
+++ funcoes/stringOla.js
@@ -0,0 +1,6 @@
+function nomeOla(leonardo) {
+
+    return 'Olá ' + leonardo + '!'
+}
+
+console.log(nomeOla('Marcos'))
