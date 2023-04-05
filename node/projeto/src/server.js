280e13ffd7b5dd04d4586a2de5dee48a75431a97
Marcos Felipe S.L <marcos.lima52@fatec.sp.gov.br>
2023-04-04 23:43:07 -0300

    PUT and DELETE - CRUD

--- node/projeto/src/server.js
+++ node/projeto/src/server.js
@@ -8,7 +8,7 @@ const bancoDeDados = require('./database')
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/produtos', (req, res, next) => {
-    res.send(bancoDeDados.getProduto())
+    res.send(bancoDeDados.getProdutos())
 })
 
 app.get('/produtos/:id', (req, res, next) => {
@@ -22,6 +22,20 @@ app.post('/produtos', (req, res, next) => {
     })
     res.send(produto) //JSON
 })
+
+app.put('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        id: req.params.id,
+        nome: req.body.nome,
+        preco: req.body.preco 
+    })
+    res.send(produto) //JSON
+})
+
+app.delete('/produtos/:id', (req, res, next) => {
+  const produto = bancoDeDados.excluirProduto(req.params.id);
+  res.send(produto); //JSON
+});
 
 app.listen(porta, () => {
     console.log(`Servidor executando na porta ${porta}.`)
