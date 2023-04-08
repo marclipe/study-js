691f550825820f0e4f72bb1b8629d1115fe1287a
Marcos Felipe S.L <marcos.lima52@fatec.sp.gov.br>
2023-04-08 00:55:32 -0300

    Review Promise rocket

--- esnext/promiseReviewRocket.js
+++ esnext/promiseReviewRocket.js
@@ -1,19 +1,40 @@
 //Exite tempo de Resposta
 //.then/.catch
 
-// const somaDoisNumeros = (a, b) => {
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             resolve(a + b);
-//         }, 2000);
-//     })
-// }
+const somaDoisNumeros = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a + b);
+        }, 2000);
+    })
+}
 
-// somaDoisNumeros(2, 3)
-// .then(soma => {console.log(soma)})
-// .catch(err => console.log(err))
-
+somaDoisNumeros(2, 3)
+.then(soma => {console.log(soma)})
+.catch(err => console.log(err))
 
 fetch("https://api.github.com/users/marclipe")
-.then(response => console.log(response))
+.then(response => {
+    //Converte para texto - text é uma Promise
+    response.json().then(body => {
+        console.log(body)
+    })
+})
 .catch(error => console.log(error))
+
+
+//Promise aninhada
+fetch("https://api.github.com/users/marclipe")
+.then(response => {
+    return response.json();
+})
+.then(body => {
+    console.log(body)
+})
+.catch(err => {
+    console.log(err)
+})
+.finally(() => {
+    console.log('Ok')
+})
+
