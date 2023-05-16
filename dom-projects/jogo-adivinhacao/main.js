fc4869ea3aff2234da075063bcdd595bd078cc94
MarcLipe <marcos.lima52@fatec.sp.gov.br>
2023-05-15 22:50:55 -0300

    DOM Events with game riddle.

--- dom-projects/jogo-adivinhacao/main.js
+++ dom-projects/jogo-adivinhacao/main.js
@@ -1,8 +1,40 @@
-const handleClick = document.querySelector('.handleClick')
+const screen1 = document.querySelector(".screen1");
+const screen2 = document.querySelector(".screen2");
+const btnTry = document.querySelector(".btnTry");
+const btnReset = document.querySelector("#btnReset");
+const randomNumber = Math.round(Math.random() * 10) /*Número randomico em memória assim que a aplicação começar*/
+let xAttempts = 1
 
-handleClick.addEventListener('click', function(event){
-    event.preventDefault() /*Para não carregar a página*/
-
-    const inputNumber = document.querySelector("#inputNumber");
-    console.log(inputNumber.value); /*Está pegando o valor no campo digitado*/
+btnTry.addEventListener('click', handleTryClick); //função callback
+btnReset.addEventListener('click', handleResetClick);
+document.addEventListener('keydown', function(e) {
+    if (e.key == 'Enter') {
+        handleResetClick();
+    }
 })
+
+function handleTryClick(event) {
+   event.preventDefault();
+
+   const inputNumber = document.querySelector("#inputNumber");
+
+   /*Verifica se acertei o número*/
+   if (Number(inputNumber.value) == randomNumber) {
+    toggleScreen();    
+    document.querySelector(
+    ".screen2 h2"
+    ).innerText = `Acertou em ${xAttempts} tentativas`;
+   }
+   inputNumber.value = ''
+   xAttempts++;
+}
+
+function handleResetClick() {
+    toggleScreen();
+    xAttempts = 1;
+}
+
+function toggleScreen() {
+    screen1.classList.toggle("hide");
+    screen2.classList.toggle("hide");
+}
